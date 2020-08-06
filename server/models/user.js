be31f76e537fa632ef1166af183ed292c853cdaf
Ierofantis <evil69669@gmail.com>
2020-08-07 00:55:15 +0300

    Reformat code

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,4 @@
-const config = { "myprivatekey": "myprivatekey" }
 const jwt = require('jsonwebtoken');
-const Joi = require('joi');
-const bcrypt = require('bcrypt-nodejs');
 
 module.exports = (sequelize, Sequelize) => {
   const user = sequelize.define('user', {
@@ -31,30 +28,10 @@ module.exports = (sequelize, Sequelize) => {
     }
   });
 
-  //custom method to generate authToken 
   user.generateAuthToken = function () {
-    const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, "myprivatekey"); //get the private key from the config file -> environment variable
+    const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, "myprivatekey");
     return token;
   }
 
-  user.comparePassword = function (passw, cb) {
-    bcrypt.compare(passw, this.password, function (err, isMatch) {
-      if (err) {
-        return cb(err);
-      }
-      cb(null, isMatch);
-    });
-  };
-
-  //function to validate user 
-  function validateUser(user) {
-    const schema = {
-      username: Joi.string().min(3).max(50).required(),
-      email: Joi.string().min(5).max(255).required().email(),
-      password: Joi.string().min(3).max(255).required()
-    };
-
-    return Joi.validate(user, schema);
-  }
   return user;
 };
